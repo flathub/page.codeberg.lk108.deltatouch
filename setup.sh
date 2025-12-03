25231aaa177c299df22c2fc6af64243bd884c6ea
d2weber <29163905+d2weber@users.noreply.github.com>
2025-12-03 17:09:03 +0100

    fix: use COMMIT_HASH for shallow clone

--- setup.sh
+++ setup.sh
@@ -16,6 +16,7 @@ cat >generated/deltatouch-git.json <<EOL
 EOL
 
 rm -rf deltatouch-shallow
-git clone --depth 1 --recurse-submodules --shallow-submodules https://codeberg.org/lk108/deltatouch deltatouch-shallow
+git clone --depth 1 --no-checkout https://codeberg.org/lk108/deltatouch deltatouch-shallow
+(cd deltatouch-shallow && git fetch --depth 1 origin "${COMMIT_HASH}" && git checkout "${COMMIT_HASH}" && git submodule update --init --recursive --depth 1)
 
 uvx --from flatpak_cargo_generator flatpak-cargo-generator deltatouch-shallow/libs/chatmail-core/Cargo.lock -o generated/sources-rust.json
